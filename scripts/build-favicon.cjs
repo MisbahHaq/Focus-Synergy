ea59ba27bb4cb73fc7fbcbaccbb300c12571dc62
Misbah Haque <misbahmuneer2003@gmail.com>
2026-09-16 15:54:16 +0500

    Minor Icon Fix

--- scripts/build-favicon.cjs
+++ scripts/build-favicon.cjs
@@ -1,15 +1,8 @@
-const sharp = require('sharp');
-const { imagesToIco } = require('png-to-ico');
+const pngToIco = require('png-to-ico').default;
 const fs = require('fs');
 
 async function main() {
-  const sizes = [256, 128, 48, 32, 16];
-  const buffers = [];
-  for (const size of sizes) {
-    const buf = await sharp('frontend/fav.png').resize(size, size).png().toBuffer();
-    buffers.push(buf);
-  }
-  const ico = await imagesToIco(buffers);
+  const ico = await pngToIco('frontend/fav.png');
   fs.writeFileSync('src-tauri/icons/favicon.ico', ico);
   console.log('Created favicon.ico, size:', ico.length, 'bytes');
 }
