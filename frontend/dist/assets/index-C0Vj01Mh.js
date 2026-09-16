d553e673eea760cb47a29025cbcb7e31e5222419
Misbah Haque <misbahmuneer2003@gmail.com>
2026-09-16 13:35:58 +0500

    Update: UI OverHaul 2.0

---
+++ frontend/dist/assets/index-C0Vj01Mh.js
@@ -0,0 +1,2 @@
+import{a as e,n as t,r as n,t as r}from"./lucide-zFRlcmyM.js";r({icons:t}),window.toggleTheme=e,n();
+//# sourceMappingURL=index-C0Vj01Mh.js.map
--- frontend/dist/assets/index-Dfu7RYkm.js
+++
@@ -1,2 +0,0 @@
-import{a as e,n as t,r as n,t as r}from"./lucide-CfMXY0bY.js";r({icons:t}),window.toggleTheme=e,n();
-//# sourceMappingURL=index-Dfu7RYkm.js.map
